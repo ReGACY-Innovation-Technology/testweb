c756d9ea9121e6086438f0849f42149e6378fcfa
uedaakihiro <Ueda.Akihiro@tsu-x.com>
2023-10-13 19:04:41 +0900

    修正

--- db/initial.sql
+++ db/initial.sql
@@ -1,13 +1,10 @@
 CREATE TABLE `hoges` (
-  `hoge_id` int(20) UNSIGNED NOT NULL,
-  `created_at` text NOT NULL
+  `hoge_id` int(20),
+  `created_at` varchar(255),
+  PRIMARY KEY (`hoge_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `hoges` (`hoge_id`, `created_at`) VALUES
 (1, 'yamadatarou'),
 (3, 'inoue'),
 (4, 'itou');
-
-ALTER TABLE `hoges`
-  MODIFY `hoge_id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
